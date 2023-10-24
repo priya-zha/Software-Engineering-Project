36d491db9a6f0bb7e9b347b8b2db9659ff6484f3
sijo <sijorejigeorge007@gmail.com>
2023-10-24 02:34:40 -0500

    Updated PermissionDenied activity to include a fix for permission retry function

--- app/src/main/java/com/example/se/PermissionDenied.java
+++ app/src/main/java/com/example/se/PermissionDenied.java
@@ -2,15 +2,23 @@ package com.example.se;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.speech.tts.TextToSpeech;
 import android.widget.Toast;
 
 public class PermissionDenied extends AppCompatActivity implements TextToSpeech.OnInitListener {
     private TextToSpeech textToSpeech;
     private boolean isTTSInitialized = false;
+
+    private static final int PERMISSION_SETTINGS_REQUEST_CODE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +32,13 @@ public class PermissionDenied extends AppCompatActivity implements TextToSpeech.
     private void showBackExitDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Confirmation");
-        builder.setMessage("Do you want to go back to the previous page or exit the app?");
-        builder.setPositiveButton("Back", new DialogInterface.OnClickListener() {
+        builder.setMessage("App needs access to the camera and microphone to function. Please enable these permissions in the app settings.");
+        builder.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 // Handle "Back" button press.
                 // In this example, we just finish the current activity.
-                finish();
+                openAppSettings();
             }
         });
         builder.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
@@ -46,6 +54,35 @@ public class PermissionDenied extends AppCompatActivity implements TextToSpeech.
         dialog.show();
 
 
+    }
+
+    private void openAppSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivityForResult(intent, PERMISSION_SETTINGS_REQUEST_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Toast.makeText(this, "request code"+requestCode, Toast.LENGTH_SHORT).show();
+
+        if (requestCode == PERMISSION_SETTINGS_REQUEST_CODE) {
+            // Check if the user has granted the permissions.
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+                // Permissions granted. Start the GetStarted activity.
+                Intent intent = new Intent(this, GetStarted.class);
+                startActivity(intent);
+            } else {
+                // Permissions still not granted.
+                // You can handle this case as needed, such as showing a message or taking appropriate action.
+                Intent intent = new Intent(this, PermissionDenied.class);
+                startActivity(intent);
+            }
+        }
     }
 
     @Override
